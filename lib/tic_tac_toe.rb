836e861ae9da3bcf00e1b9823ce4cbefc155beab
Aaron Samuel <aasamuelj@gmail.com>
2017-09-23 12:23:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,6 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
