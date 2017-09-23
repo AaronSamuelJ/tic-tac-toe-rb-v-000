acc7d97b388e26fbbe450da62d616c32a4ccd767
Aaron Samuel <aasamuelj@gmail.com>
2017-09-23 12:33:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,8 +30,7 @@ def move(board, index, char)
   board[index] = char
 end
 def valid_move?(board, position)
-  binding.pry
-  position.to_i.between?(1,9) && board[position] != "X" or board[position] != "O"
+  position.to_i.between?(1,9) && board[position] != "X" && board[position] != "O"
 end
 def won?(board)
   array = false
